7e6a3804fbdad4a5addbada3ee968cdbbed9686a
liuaiying <1425577366@qq.com>
2022-07-30 17:23:35 +0800

    初次提交代码

---
+++ src/settings.js
@@ -0,0 +1,17 @@
+module.exports = {
+// 网站标题,修改重新起服务
+  title: 'hrhass',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  // 左侧logo
+  sidebarLogo: true
+}
---
+++ src/store/modules/user.js
@@ -0,0 +1,7 @@
+export default {
+  namespaced: true,
+  state: {},
+  mutations: {},
+  actions: {}
+}
+// 其中app.js模块和settings.js模块，功能已经完备，不需要再进行修改。 user.js模块是我们后期需要重点开发的内容，所以这里我们将user.js里面的内容删除，并且导出一个默认配置
